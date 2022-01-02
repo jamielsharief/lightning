baf972a052fed80c53d7620d0544261dab163c24
Jamiel Sharief <20553479+jamielsharief@users.noreply.github.com>
2022-01-02 13:16:19 +0100

    Added Cookie Object

---
+++ src/Http/Cookie/Cookie.php
@@ -0,0 +1,292 @@
+<?php declare(strict_types=1);
+/**
+ * LightningPHP
+ * Copyright 2021 Jamiel Sharief.
+ *
+ * Licensed under The MIT License
+ * The above copyright notice and this permission notice shall be included in all copies or substantial
+ * portions of the Software.
+ *
+ * @copyright   Copyright (c) Jamiel Sharief
+ * @license     https://opensource.org/licenses/mit-license.php MIT License
+ */
+
+namespace Lightning\Http\Cookie;
+
+use Stringable;
+use InvalidArgumentException;
+
+/**
+ * Cookie
+ *
+ * @internal isHttpOnly vs getSecure, is secure I am not sure about.
+ */
+class Cookie implements Stringable
+{
+    protected string $name;
+    protected string $value;
+
+    /**
+     * Set to expire when browser closes
+     *
+     * @var integer
+     */
+    protected int $maxAge = -1;
+
+    /**
+     * By default cookies on whole domain (javascript default is page ownly)
+     *
+     * @var string
+     */
+    protected string $path = '/';
+    protected string $domain = '';
+
+    protected bool $secure = false;
+    protected bool $httpOnly = false;
+
+    protected string $sameSite = '';
+
+    /**
+     * Constructor
+     *
+     * @see https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Set-Cookie#Directives
+     *
+     * @param string $name
+     * @param string $value
+     */
+    public function __construct(string $name, string $value)
+    {
+        /// space, tab, ( ) < > @ , ; : \ " / [ ] ? = { }
+        if ((bool) preg_match('#[\s\t\(\)\[\]<>@,;:?="/\\\]#', $name)) {
+            throw new InvalidArgumentException(sprintf('Invalid cookie name `%s`', $name));
+        }
+        $this->name = $name;
+        $this->value = $value;
+    }
+
+    /**
+     * Gets the name of this cookie
+     *
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * Gets the value of this cookie
+     *
+     * @return string
+     */
+    public function getValue(): string
+    {
+        return $this->value;
+    }
+
+    /**
+     * Sets the value of this cookie
+     *
+     * @param string $value
+     * @return self
+     */
+    public function setValue(string $value): self
+    {
+        $this->value = $value;
+
+        return $this;
+    }
+
+    /**
+     * Checks if the cookie is marked as HTTP only
+     *
+     * @see https://developer.mozilla.org/en-US/docs/Web/HTTP/Cookies
+     *
+     * @return boolean
+     */
+    public function isHttpOnly(): bool
+    {
+        return $this->httpOnly;
+    }
+
+    /**
+     * Set the value of httpOnly
+     *
+     * @param bool $httpOnly
+     *
+     * @return self
+     */
+    public function setHttpOnly(bool $httpOnly): self
+    {
+        $this->httpOnly = $httpOnly;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of secure
+     *
+     * @return bool
+     */
+    public function getSecure(): bool
+    {
+        return $this->secure;
+    }
+
+    /**
+     * Set the value of secure
+     *
+     * @param bool $secure
+     *
+     * @return self
+     */
+    public function setSecure(bool $secure): self
+    {
+        $this->secure = $secure;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of path
+     *
+     * @return string
+     */
+    public function getPath(): string
+    {
+        return $this->path;
+    }
+
+    /**
+     * Set the value of path
+     *
+     * @param string $path
+     *
+     * @return self
+     */
+    public function setPath(string $path): self
+    {
+        $this->path = $path;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of domain
+     *
+     * @return string
+     */
+    public function getDomain(): string
+    {
+        return $this->domain;
+    }
+
+    /**
+     * Set the value of domain
+     *
+     * @param string $domain
+     *
+     * @return self
+     */
+    public function setDomain(string $domain): self
+    {
+        $this->domain = $domain;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of maxAge
+     *
+     * @return int
+     */
+    public function getMaxAge(): int
+    {
+        return $this->maxAge;
+    }
+
+    /**
+     * Set the value of maxAge
+     *
+     * @param int $maxAge
+     *
+     * @return self
+     */
+    public function setMaxAge(int $maxAge): self
+    {
+        $this->maxAge = $maxAge;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of sameSite
+     *
+     * @return string
+     */
+    public function getSameSite(): string
+    {
+        return $this->sameSite;
+    }
+
+    /**
+     * Set the value of sameSite
+     *
+     * @see https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Set-Cookie/SameSite
+     *
+     * @param string $sameSite
+     *
+     * @return self
+     */
+    public function setSameSite(string $sameSite): self
+    {
+        $this->sameSite = $sameSite;
+
+        return $this;
+    }
+
+    /**
+     * Gets a string representation of this cookie
+     *
+     * @return string
+     */
+    public function toString(): string
+    {
+        $out = [];
+
+        $out[] = sprintf('%s=%s', $this->name, urlencode($this->value));
+        $out[] = sprintf('max-age=%s', $this->maxAge);
+
+        if (! empty($this->path)) {
+            $out[] = sprintf('path=%s', $this->path);
+        }
+
+        if (! empty($this->domain)) {
+            $out[] = sprintf('domain=%s', $this->domain);
+        }
+
+        if (! empty($this->sameSite)) {
+            $out[] = sprintf('samesite=%s', $this->sameSite);
+        }
+
+        if ($this->secure) {
+            $out[] = 'secure';
+        }
+
+        if ($this->httpOnly) {
+            $out[] = 'httponly';
+        }
+
+        return implode('; ', $out);
+    }
+
+    /**
+     * Stringable
+     *
+     * @return string
+     */
+    public function __toString(): string
+    {
+        return $this->toString();
+    }
+}
---
+++ tests/TestCase/Http/Cookie/CookieTest.php
@@ -0,0 +1,129 @@
+<?php declare(strict_types=1);
+
+namespace Lightning\Test\Http\Exception;
+
+use InvalidArgumentException;
+use PHPUnit\Framework\TestCase;
+use Lightning\Http\Cookie\Cookie;
+
+final class CookieTest extends TestCase
+{
+    public function testInvalidName()
+    {
+        $regex = '#[\s\t\(\)\[\]<>@,;:?="/\\\]#';
+
+        // some quick sanity checks
+        $this->assertMatchesRegularExpression($regex, 'foo bar');
+        $this->assertMatchesRegularExpression($regex, 'foo  bar');
+        $this->assertMatchesRegularExpression($regex, 'foo=bar');
+        $this->assertMatchesRegularExpression($regex, 'foo,bar');
+        $this->assertMatchesRegularExpression($regex, 'foo;bar');
+        $this->assertMatchesRegularExpression($regex, 'foo[bar]');
+
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('Invalid cookie name `foo bar`');
+
+        new Cookie('foo bar', '');
+    }
+    public function testGetName(): void
+    {
+        $cookie = new Cookie('foo', 'bar');
+        $this->assertEquals('foo', $cookie->getName());
+    }
+
+    public function testValue(): void
+    {
+        $cookie = new Cookie('foo', 'bar');
+        $this->assertEquals('bar', $cookie->getValue());
+
+        $this->assertEquals('', $cookie->setValue('')->getValue());
+    }
+
+    public function testMaxAge(): void
+    {
+        $cookie = new Cookie('foo', 'bar');
+        $this->assertEquals(-1, $cookie->getMaxAge());
+        $this->assertEquals(3600, $cookie->setMaxAge(3600)->getMaxAge());
+    }
+
+    public function testHttpOnly(): void
+    {
+        $cookie = new Cookie('foo', 'bar');
+
+        $this->assertFalse($cookie->isHttpOnly());
+        $this->assertInstanceOf(Cookie::class, $cookie->setHttpOnly(true));
+        $this->assertTrue($cookie->isHttpOnly());
+    }
+
+    public function testSecure(): void
+    {
+        $cookie = new Cookie('foo', 'bar');
+
+        $this->assertFalse($cookie->getSecure());
+        $this->assertInstanceOf(Cookie::class, $cookie->setSecure(true));
+        $this->assertTrue($cookie->getSecure());
+    }
+
+    public function testPath(): void
+    {
+        $cookie = new Cookie('foo', 'bar');
+        $this->assertEquals('/', $cookie->getPath());
+        $this->assertEquals('', $cookie->setPath('')->getPath());
+    }
+
+    public function testDomain(): void
+    {
+        $cookie = new Cookie('foo', 'bar');
+        $this->assertEquals('/', $cookie->getPath());
+        $this->assertEquals('', $cookie->setPath('')->getPath());
+    }
+
+    public function testSetSameSite(): void
+    {
+        $cookie = new Cookie('foo', 'bar');
+        $this->assertInstanceOf(Cookie::class, $cookie->setSameSite('Lax'));
+        $this->assertEquals('Lax', $cookie->getSameSite());
+    }
+
+    public function testToString(): void
+    {
+        $cookie = new Cookie('foo', 'bar');
+        $this->assertEquals('foo=bar; max-age=-1; path=/', $cookie->toString());
+        $this->assertEquals('foo=bar; max-age=-1; path=/', (string) $cookie);
+    }
+
+    public function testToStringDomain(): void
+    {
+        $cookie = new Cookie('foo', 'bar');
+        $this->assertEquals('foo=bar; max-age=-1; path=/', (string) $cookie); // no domain
+
+        $cookie->setDomain('example.com');
+        $this->assertEquals('foo=bar; max-age=-1; path=/; domain=example.com', (string) $cookie);
+    }
+
+    public function testToStringHttpOnly(): void
+    {
+        $cookie = new Cookie('foo', 'bar');
+        $this->assertEquals('foo=bar; max-age=-1; path=/', (string) $cookie); // no domain
+
+        $cookie->setHttpOnly(true);
+        $this->assertEquals('foo=bar; max-age=-1; path=/; httponly', (string) $cookie); // no domain
+    }
+    public function testToSecure(): void
+    {
+        $cookie = new Cookie('foo', 'bar');
+        $this->assertEquals('foo=bar; max-age=-1; path=/', (string) $cookie); // no domain
+
+        $cookie->setSecure(true);
+        $this->assertEquals('foo=bar; max-age=-1; path=/; secure', (string) $cookie); // no domain
+    }
+
+    public function testToSameSite(): void
+    {
+        $cookie = new Cookie('foo', 'bar');
+        $this->assertEquals('foo=bar; max-age=-1; path=/', (string) $cookie); // no domain
+
+        $cookie->setSameSite('Lax');
+        $this->assertEquals('foo=bar; max-age=-1; path=/; samesite=Lax', (string) $cookie); // no domain
+    }
+}

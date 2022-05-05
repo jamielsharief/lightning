3dfc514559c5c8be563d5640aadde5c2ddff739f
Jamiel Sharief <20553479+jamielsharief@users.noreply.github.com>
2022-05-05 13:57:08 +0200

    removed temp code

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,5 +3,4 @@
 use Lightning\Dotenv\Dotenv;
 
 require dirname(__DIR__) . '/config/autoload.php';
-include dirname(__DIR__) . '/src/Dotenv/functions.php'; // TODO: temp
 (new Dotenv(dirname(__DIR__)))->load();

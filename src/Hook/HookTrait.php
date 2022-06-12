fa7ec72f3a00a2d4afe6ea3c9b6bbb65f27b010e
Jamiel Sharief <20553479+jamielsharief@users.noreply.github.com>
2022-06-12 06:58:01 +0200

    Brought back hooks as this is much better and flexible for modify behaviors

---
+++ src/Hook/HookTrait.php
@@ -0,0 +1,56 @@
+<?php declare(strict_types=1);
+/**
+ * LightningPHP
+ * Copyright 2021 - 2022 Jamiel Sharief.
+ *
+ * Licensed under The MIT License
+ * The above copyright notice and this permission notice shall be included in all copies or substantial
+ * portions of the Software.
+ *
+ * @copyright   Copyright (c) Jamiel Sharief
+ * @license     https://opensource.org/licenses/mit-license.php MIT License
+ */
+
+namespace Lightning\Hook;
+
+use InvalidArgumentException;
+
+trait HookTrait
+{
+    protected array $registeredHooks = [];
+
+    /**
+     * Registers a hook
+     */
+    public function registerHook(string $name, string $method): static
+    {
+        if (! method_exists($this, $method)) {
+            throw new InvalidArgumentException(sprintf('Hook method `%s` does not exist', $method)); // This is not fine
+        }
+
+        if (! isset($this->registeredHooks[$name])) {
+            $this->registeredHooks[$name] = [];
+        }
+
+        $this->registeredHooks[$name][] = $method;
+
+        return $this;
+    }
+
+    /**
+     * Trigger a hook and calls the register hooks method the first time, avoid having to use
+     * in the constructor method.
+     */
+    public function triggerHook(string $name, array $arguments = [], bool $isStoppable = true): bool
+    {
+        $hooks = $this->registeredHooks[$name] ?? [];
+
+        foreach ($hooks as $method) {
+            if (call_user_func_array([$this,$method], $arguments) === false && $isStoppable) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
---
+++ src/Hook/README.md
@@ -0,0 +1,37 @@
+# Hooks
+
+Hooks package allows you to modify or extend the behavior of an object using the [Interceptor pattern](https://en.wikipedia.org/wiki/Interceptor_pattern).
+
+## Usage
+
+Register a hook to a method or multiple methods, by default all hooks can cancel the behavior that is being hooked into. 
+
+The `triggerHook` method always returns true even if there are no hooks registered, it only returns false if the hook was stoppable and the method returned `false`.
+
+```php
+class MyObject
+{
+    use HookTrait; 
+
+    public function __construct()
+    {
+        $this->registerHook('beforeFind','doSomething');
+    }
+
+    protected function doSomething(array $criteria)
+    {
+        return !empty($criteria);
+    }
+
+    public function find(array $criteria) : bool 
+    {
+        if(!$this->triggerHook('beforeFind',[$criteria])){
+            return false;
+        }
+        $result = $this->dataSource->find($criteria);
+
+        //.. do something
+        $this->triggerHook('afterFind',[$result, $criteria], false);
+    }
+}
+```
